5f4b32ec6b690af6de1069fb7cd852b772b8ebc9
AlanConstantino <constantinoalan98@gmail.com>
2020-08-18 13:42:44 -0700

    First refactor (tried to make more readable)

--- server/app.js
+++ server/app.js
@@ -28,9 +28,10 @@ const port = 3000
 // HELPER FUNCTIONS
 const views = (file) => nodePath.normalize(`${__dirname}/views/${file}`)
 const removeEncodedUrlSpace = string => string.replace(/%20/g, ' ')
-const isValid = input => ((input !== undefined) && (input !== '') && (input !== null))
-const isArrayValid = array => ((array !== undefined) && (array !== null) && (array.length !== 0))
-const fileExists = path => {
+const isValid = (input) => ((input !== undefined) && (input !== '') && (input !== null))
+const isArrayValid = (array) => ((array !== undefined) && (array !== null) && (array.length !== 0))
+const filePathExists = (path) => fs.existsSync(path)
+const fileExists = (path) => {
   try {
     fs.accessSync(path, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK)
     console.log('File exists, and can be read, and can be written to.')
@@ -46,32 +47,36 @@ const storagePath = (() => {
   const originalPath = __dirname.split(nodePath.sep)
   originalPath.pop()
   const systemAgnosticPath = nodePath.normalize(nodePath.join(...originalPath, '/users/username/home'))
-  const containsBeginningSlash = systemAgnosticPath[0] === '/' || systemAgnosticPath[0] === '\\'
-  if (!containsBeginningSlash) return nodePath.normalize('/' + systemAgnosticPath)
+  const pathContainsBeginningSlash = systemAgnosticPath[0] === nodePath.sep
+  if (!pathContainsBeginningSlash) return nodePath.normalize('/' + systemAgnosticPath)
   return systemAgnosticPath
 })()
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const path = req.headers.path
-    if (path !== undefined && path !== '') {
-      const newPath = nodePath.normalize(removeEncodedUrlSpace(path).replace('?path=', ''))
-      const newStoragePath = storagePath.replace('home', '') + newPath.slice(1, newPath.length)
-      return cb(null, newStoragePath)
-    }
-    return cb(null, storagePath)
-  },
-  filename: (req, file, cb) => {
-    const fileExists = fs.existsSync(nodePath.join(storagePath, file.originalname))
-    const [, ext] = file.mimetype.split('/')
-    if (fileExists) {
-      const [name,] = file.originalname.split(`.${ext}`)
-      return cb(null, `${name}-copy.${ext}`)
-    }
-    return cb(null, file.originalname)
+// for multer (storage object)
+// only gets called when uploading files
+const destination = (req, file, cb) => {
+  const path = req.headers.path
+  if (isValid(path)) {
+    const newPath = nodePath.normalize(removeEncodedUrlSpace(path).replace('?path=', ''))
+    const newStoragePath = storagePath.replace('home', '') + newPath.slice(1, newPath.length)
+    return cb(null, newStoragePath)
   }
-})
-const upload = multer({ storage: storage })
+  return cb(null, storagePath)
+}
+// for multer (storage object)
+// only gets called when uploading files
+const filename = (req, file, cb) => {
+  const pathExists = filePathExists(nodePath.join(storagePath, file.originalname))
+  const [, ext] = file.mimetype.split(nodePath.sep)
+  if (pathExists) {
+    const [name,] = file.originalname.split(`.${ext}`)
+    return cb(null, `${name}-copy.${ext}`)
+  }
+  return cb(null, file.originalname)
+}
+
+const storage = multer.diskStorage({ destination, filename })
+const upload = multer({ storage })
 
 // username and password
 const user = {
@@ -108,8 +113,8 @@ app.get('/upload', (req, res) => {
 })
 
 app.get('/downloadZip', (req, res) => {
-  const noFiles = req.session.files === undefined || req.session.files.length === 0
-  if (noFiles) return res.status(400).send()
+  const filesAreAvailableToDownload = isArrayValid(req.session.files)
+  if (!filesAreAvailableToDownload) return res.status(400).send()
   const filesToDownload = {
     files: req.session.files,
     filename: 'files.zip'
@@ -124,8 +129,11 @@ app.get('/createFolder', (req, res) => {
 
   const fullPath = nodePath.normalize(storagePath.replace('home', pathName) + dirName)
   fs.mkdir(fullPath, (err) => {
-    if (err) return res.status(400).redirect(pathName)
-    else return res.status(200).redirect(pathName)
+    if (err) {
+      return res.status(400).redirect(pathName)
+    } else {
+      return res.status(200).redirect(pathName)
+    }
   })
 })
 
@@ -142,24 +150,30 @@ app.post('/loginAuth', async (req, res) => {
     if (usernamesMatch && passwordsMatch) {
       req.session.isUserAuth = true
       return res.status(200).redirect('/menu')
+    } else {
+      return res.status(401).redirect('/')
     }
-    return res.status(401).redirect('/')
-  } catch {
-    return res.status(500).send()
+  } catch (err) {
+    return res.status(500).send('An error has occured:\n' + err)
   }
 })
 
 app.post('/uploadAuth', upload.array('uploaded-files'), (req, res) => {
-  if (isArrayValid(req.files)) return res.status(200).send()
-  return res.status(500).redirect('/upload')
+  const filesAreAvailableToUpload = isArrayValid(req.files)
+  if (!filesAreAvailableToUpload) return res.status(500).redirect('/upload')
+  return res.status(200).send()
 })
 
 app.post('/download', (req, res) => {
-  if (!isArrayValid(req.body.fileNames)) return res.status(400).send('There is no list of files.')
+  const listOfFilesExist = isArrayValid(req.body.fileNames)
+  if (!listOfFilesExist) return res.status(400).send('There is no list of files to download.')
   const files = []
-  req.body.fileNames.forEach(name => files.push({ path: `${storagePath}${name}`, name }))
+  req.body.fileNames.forEach(name => {
+    const path = `${storagePath}${name}`
+    files.push({ path, name })
+  })
   req.session.files = files
-  return res.status(200).send()
+  return res.status(200).send('Files downloaded successfully.')
 })
 
 app.post('/moveTo', (req, res) => {
@@ -174,45 +188,42 @@ app.post('/moveTo', (req, res) => {
 
   const dir = nodePath.normalize(storagePath + destinationDir.replace('/home', '/'))
 
-  fs.access(dir, (err) => {
-    if (err) {
-      // maybe create the folder/path instead of returning an error
-      return res.status(400).send(`No directory by the name of "${err.path}" exits.`)
-    } else {
-      listOfFiles.forEach(file => {
-        const splitPath = file.split(nodePath.sep)
-        const fileName = splitPath[splitPath.length - 1]
+  if (!fileExists(dir)) {
+    return res.status(400).send('The path you inputted is incorrect or does not exist, try again.')
+  } else {
+    // if errors occur, consider changing "forEach" to "every"
+    listOfFiles.forEach(file => {
+      const splitPath = file.split(nodePath.sep)
+      const fileName = splitPath[splitPath.length - 1]
 
-        const oldPath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file))
-        const newPath = nodePath.normalize(dir + '/' + fileName)
+      const oldPath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file))
+      const newPath = nodePath.normalize(dir + '/' + fileName)
 
-        const duplicatedDirectory = nodePath.normalize(`/${fileName}/${fileName}`)
-        const isDuplicateDirectory = newPath.endsWith(duplicatedDirectory)
-        if (isDuplicateDirectory) {
-          return res.status(400).send('Can\'t move a folder into itself.\n')
+      const duplicatedDirectory = nodePath.normalize(`/${fileName}/${fileName}`)
+      const isDuplicateDirectory = newPath.endsWith(duplicatedDirectory)
+      if (isDuplicateDirectory) {
+        return res.status(400).send('Can\'t move a folder into itself.')
+      }
+
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+          return res.status(400).send('The path you inputted is incorrect, try again.\n' + err)
         }
-
-        fs.rename(oldPath, newPath, (err) => {
-          if (err) {
-            // may be able to get rid of this if I remove fs.access altogether
-            return res.status(400).send('The path you inputted is incorrect, try again.\n' + err)
-          } else {
-            return res.status(200).send('Successfully moved file(s).')
-          }
-        })
       })
-    }
-  })
+    })
+    return res.status(200).send('Successfully moved file(s).')
+  }
 })
 
 // called whenever 'Delete' button is clicked with checkmarked files
 app.delete('/deleteMultiple', (req, res) => {
   const files = req.body.fileNames
-  if (!isArrayValid(files)) return res.status(400).send('There are no files to delete.')
+  const listOfFilesExist = isArrayValid(files)
+  if (!listOfFilesExist) return res.status(400).send('There are no files to delete.')
 
   files.forEach(file => {
     const filePath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file))
-    const isDirectory = fs.existsSync(filePath) && fs.lstatSync(filePath).isDirectory();
+    const isDirectory = filePathExists(filePath) && fs.lstatSync(filePath).isDirectory()
     if (isDirectory) {
       fs.rmdir(filePath, { recursive: true }, (err) => {
         if (err) {
@@ -226,15 +237,13 @@ app.delete('/deleteMultiple', (req, res) => {
     } else {
       fs.unlink(filePath, (err) => {
         if (err) {
-          console.log('Error in deleting file(s).')
+          console.log('Error in deleting file(s).\n' + err)
           return res.status(400).send('Error in deleting file(s).\n' + err)
-        } else {
-          console.log('File(s) have been deleted successfully.')
-          return res.status(200).send('File(s) have been deleted successfully.')
         }
       })
     }
   })
+  return res.status(200).send('File(s) have been deleted successfully.')
 })
 
 // only gets called when you upload a file then click on the remove link attached to dropzone
@@ -242,11 +251,13 @@ app.delete('/deleteFile', (req, res) => {
   let pathToFile = ''
   const fileName = req.body.fileName
   if (!isValid(fileName)) return res.status(500).send('File does not exist.')
-  if (req.query.path === undefined || req.query.path === '') {
+
+  if (isValid(req.query.path)) {
     pathToFile = nodePath.normalize(storagePath + '/' + fileName)
   } else {
     pathToFile = nodePath.normalize(`${storagePath.replace('home', '')}${req.query.path.replace('?path=')}${fileName}`)
   }
+
   fs.unlink(pathToFile, (err) => {
     if (err) {
       return res.status(500).send(err)
