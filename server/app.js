6441c631b4d7f74279e0f75131b1e3ae5b8c6345
AlanConstantino <constantinoalan98@gmail.com>
2020-08-18 20:59:08 -0700

    Added JSLint to project

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 // TODO:
 // - Make the process of making a new username and password easier for the user
 // - Move the user object into an excel document to act as a psuedo database so it isn't stored in the source code
@@ -11,131 +12,148 @@
 //   - Fixed bug but have not tested on Windows/Linux
 
 // require
-const express = require('express')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
-const session = require('express-session')
-const serveIndex = require('serve-index')
-const multer = require('multer')
-const fs = require('fs')
-const nodePath = require('path')
-const zip = require('express-easy-zip')
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const serveIndex = require('serve-index');
+const multer = require('multer');
+const fs = require('fs');
+const nodePath = require('path');
+const zip = require('express-easy-zip');
 
 // variables
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // HELPER FUNCTIONS
-const views = (file) => nodePath.normalize(`${__dirname}/views/${file}`)
-const removeEncodedUrlSpace = string => string.replace(/%20/g, ' ')
-const isValid = (input) => ((input !== undefined) && (input !== '') && (input !== null))
-const isArrayValid = (array) => ((array !== undefined) && (array !== null) && (array.length !== 0))
-const filePathExists = (path) => fs.existsSync(path)
+const views = (file) => nodePath.normalize(`${__dirname}/views/${file}`);
+const removeEncodedUrlSpace = (string) => string.replace(/%20/g, ' ');
+const isValid = (input) => ((input !== undefined) && (input !== '') && (input !== null));
+const isArrayValid = (array) => {
+  return ((array !== undefined) && (array !== null) && (array.length !== 0));
+};
+const filePathExists = (path) => {
+  return fs.existsSync(path);
+};
 const fileExists = (path) => {
   try {
-    fs.accessSync(path, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK)
-    console.log('File exists, and can be read, and can be written to.')
-    return true
+    fs.accessSync(path, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+    console.log('File exists, and can be read, and can be written to.');
+    return true;
   } catch (error) {
-    console.log('File does not exist, or cannot be read, or cannot be written to.')
-    return false
+    console.log('File does not exist, or cannot be read, or cannot be written to.');
+    return false;
   }
-}
+};
 
 // making the storage path system agnostic
 const storagePath = (() => {
-  const originalPath = __dirname.split(nodePath.sep)
-  originalPath.pop()
-  const systemAgnosticPath = nodePath.normalize(nodePath.join(...originalPath, '/users/username/home'))
-  const pathContainsBeginningSlash = systemAgnosticPath[0] === nodePath.sep
-  if (!pathContainsBeginningSlash) return nodePath.normalize('/' + systemAgnosticPath)
-  return systemAgnosticPath
-})()
+  const originalPath = __dirname.split(nodePath.sep);
+  originalPath.pop();
+  const systemAgnosticPath = nodePath.normalize(nodePath.join(...originalPath, '/users/username/home'));
+  const pathContainsBeginningSlash = systemAgnosticPath[0] === nodePath.sep;
+  if (!pathContainsBeginningSlash) {
+    return nodePath.normalize('/' + systemAgnosticPath);
+  }
+  return systemAgnosticPath;
+})();
 
 // for multer (storage object)
 // only gets called when uploading files
 const destination = (req, file, cb) => {
-  const path = req.headers.path
+  console.log('DESTINATION');
+  const path = req.headers.path;
   if (isValid(path)) {
-    const newPath = nodePath.normalize(removeEncodedUrlSpace(path).replace('?path=', ''))
-    const newStoragePath = storagePath.replace('home', '') + newPath.slice(1, newPath.length)
-    return cb(null, newStoragePath)
+    const newPath = nodePath.normalize(removeEncodedUrlSpace(path).replace('?path=', ''));
+    const newStoragePath = storagePath.replace('home', '') + newPath.slice(1, newPath.length);
+    return cb(null, newStoragePath);
   }
-  return cb(null, storagePath)
-}
+  return cb(null, storagePath);
+};
 // for multer (storage object)
 // only gets called when uploading files
 const filename = (req, file, cb) => {
-  const pathExists = filePathExists(nodePath.join(storagePath, file.originalname))
-  const [, ext] = file.mimetype.split(nodePath.sep)
+  console.log('FILENAME');
+  const pathExists = filePathExists(nodePath.join(storagePath, file.originalname));
+  const [, ext] = file.mimetype.split(nodePath.sep);
   if (pathExists) {
-    const [name,] = file.originalname.split(`.${ext}`)
-    return cb(null, `${name}-copy.${ext}`)
+    const [name] = file.originalname.split(`.${ext}`);
+    console.log(name);
+    return cb(null, `${name}-copy.${ext}`);
   }
-  return cb(null, file.originalname)
-}
+  return cb(null, file.originalname);
+};
 
-const storage = multer.diskStorage({ destination, filename })
-const upload = multer({ storage })
+const storage = multer.diskStorage({ destination, filename });
+const upload = multer({ storage });
 
 // username and password
 const user = {
   username: 'username',
   password: '$2b$10$6EwITJ7wXFDxUR0wquEg6enrkQnGGJCd2UQZZzAFdxwCE6HsUXojO'
-}
+};
 
 app.use(session({
   secret: '588673a595e29fcd75e4567768d0f601',
   resave: false,
   saveUninitialized: false
-}))
-app.use(express.static(nodePath.normalize(__dirname + '/public')))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/home', express.static(storagePath), serveIndex(storagePath, { 'template': `${views('home.html')}` }))
-app.use(bodyParser.json())
-app.use(bodyParser.text())
-app.use(bodyParser.raw())
-app.use(zip())
+}));
+app.use(express.static(nodePath.normalize(__dirname + '/public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/home', express.static(storagePath), serveIndex(storagePath, { 'template': `${views('home.html')}` }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.raw());
+app.use(zip());
 
-app.get('/', redirectToMenuIfLoggedIn, (req, res) => res.sendFile(views('login.html')))
+app.get('/', redirectToMenuIfLoggedIn, (req, res) => res.sendFile(views('login.html')));
 
-app.get('/menu', (req, res) => res.sendFile(views('menu.html')))
+app.get('/menu', (req, res) => res.sendFile(views('menu.html')));
 
 app.get('/logout', (req, res) => {
   req.session.destroy((err) => {
-    if (err) return res.status(500).send(err)
-    return res.status(200).redirect('/')
-  })
-})
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.status(200).redirect('/');
+  });
+});
 
 app.get('/upload', (req, res) => {
-  res.sendFile(views('/upload.html'))
-})
+  res.sendFile(views('/upload.html'));
+});
 
 app.get('/downloadZip', (req, res) => {
-  const filesAreAvailableToDownload = isArrayValid(req.session.files)
-  if (!filesAreAvailableToDownload) return res.status(400).send()
+  const filesAreAvailableToDownload = isArrayValid(req.session.files);
+  if (!filesAreAvailableToDownload) {
+    return res.status(400).send();
+  }
   const filesToDownload = {
     files: req.session.files,
     filename: 'files.zip'
-  }
-  res.zip(filesToDownload)
-})
+  };
+  res.zip(filesToDownload);
+});
 
 app.get('/createFolder', (req, res) => {
-  const pathName = req.query.path
-  const dirName = req.query.dirName
-  if (!isValid(pathName) && !isValid(dirName)) return res.status(400).redirect('/home')
+  const pathName = req.query.path;
+  const dirName = req.query.dirName;
+  if (!isValid(pathName) && !isValid(dirName)) {
+    return res.status(400).redirect('/home');
+  }
 
-  const fullPath = nodePath.normalize(storagePath.replace('home', pathName) + dirName)
+  const fullPath = nodePath.normalize(storagePath.replace('home', pathName) + dirName);
   fs.mkdir(fullPath, (err) => {
     if (err) {
-      return res.status(400).redirect(pathName)
+      console.log('Error in creating folder.');
+      return res.status(400).redirect(pathName);
     } else {
-      return res.status(200).redirect(pathName)
+      console.log('Folder has been created successfully!');
+      return res.status(200).redirect(pathName);
     }
-  })
-})
+  });
+});
 
 app.post('/loginAuth', async (req, res) => {
   // MAKING A PASSWORD
@@ -145,141 +163,153 @@ app.post('/loginAuth', async (req, res) => {
   //     console.log(hashedPassword)
   // })
   try {
-    const usernamesMatch = req.body.username === user.username
-    const passwordsMatch = await bcrypt.compare(req.body.password, user.password)
+    const usernamesMatch = req.body.username === user.username;
+    const passwordsMatch = await bcrypt.compare(req.body.password, user.password);
     if (usernamesMatch && passwordsMatch) {
-      req.session.isUserAuth = true
-      return res.status(200).redirect('/menu')
+      req.session.isUserAuth = true;
+      return res.status(200).redirect('/menu');
     } else {
-      return res.status(401).redirect('/')
+      return res.status(401).redirect('/');
     }
   } catch (err) {
-    return res.status(500).send('An error has occured:\n' + err)
+    return res.status(500).send('An error has occured:\n' + err);
   }
-})
+});
 
 app.post('/uploadAuth', upload.array('uploaded-files'), (req, res) => {
-  const filesAreAvailableToUpload = isArrayValid(req.files)
-  if (!filesAreAvailableToUpload) return res.status(500).redirect('/upload')
-  return res.status(200).send()
-})
+  const filesAreAvailableToUpload = isArrayValid(req.files);
+  if (!filesAreAvailableToUpload) {
+    return res.status(500).redirect('/upload');
+  }
+  return res.status(200).send();
+});
 
 app.post('/download', (req, res) => {
-  const listOfFilesExist = isArrayValid(req.body.fileNames)
-  if (!listOfFilesExist) return res.status(400).send('There is no list of files to download.')
-  const files = []
-  req.body.fileNames.forEach(name => {
-    const path = `${storagePath}${name}`
-    files.push({ path, name })
-  })
-  req.session.files = files
-  return res.status(200).send('Files downloaded successfully.')
-})
+  const listOfFilesExist = isArrayValid(req.body.fileNames);
+  if (!listOfFilesExist) {
+    return res.status(400).send('There is no list of files to download.');
+  }
+  const files = [];
+  req.body.fileNames.forEach((name) => {
+    const path = `${storagePath}${name}`;
+    files.push({ path, name });
+  });
+  req.session.files = files;
+  return res.status(200).send('Files downloaded successfully.');
+});
 
 app.post('/moveTo', (req, res) => {
-  const destinationDir = req.body.destinationDir
-  const listOfFiles = req.body.listOfFiles
+  const destinationDir = req.body.destinationDir;
+  const listOfFiles = req.body.listOfFiles;
   if (!isValid(destinationDir)) {
-    return res.status(400).send('No destination directory specified')
+    return res.status(400).send('No destination directory specified');
   }
   if (!isArrayValid(listOfFiles)) {
-    return res.status(400).send('No file(s) are present to move.')
+    return res.status(400).send('No file(s) are present to move.');
   }
 
-  const dir = nodePath.normalize(storagePath + destinationDir.replace('/home', '/'))
+  const dir = nodePath.normalize(storagePath + destinationDir.replace('/home', '/'));
 
   if (!fileExists(dir)) {
-    return res.status(400).send('The path you inputted is incorrect or does not exist, try again.')
+    return res.status(400).send('The path you inputted is incorrect or does not exist, try again.');
   } else {
     // if errors occur, consider changing "forEach" to "every"
-    listOfFiles.forEach(file => {
-      const splitPath = file.split(nodePath.sep)
-      const fileName = splitPath[splitPath.length - 1]
+    listOfFiles.forEach((file) => {
+      const splitPath = file.split(nodePath.sep);
+      const fileName = splitPath[splitPath.length - 1];
 
-      const oldPath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file))
-      const newPath = nodePath.normalize(dir + '/' + fileName)
+      const oldPath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file));
+      const newPath = nodePath.normalize(dir + '/' + fileName);
 
-      const duplicatedDirectory = nodePath.normalize(`/${fileName}/${fileName}`)
-      const isDuplicateDirectory = newPath.endsWith(duplicatedDirectory)
+      const duplicatedDirectory = nodePath.normalize(`/${fileName}/${fileName}`);
+      const isDuplicateDirectory = newPath.endsWith(duplicatedDirectory);
       if (isDuplicateDirectory) {
-        return res.status(400).send('Can\'t move a folder into itself.')
+        return res.status(400).send('Can\'t move a folder into itself.');
       }
 
       fs.rename(oldPath, newPath, (err) => {
         if (err) {
-          return res.status(400).send('The path you inputted is incorrect, try again.\n' + err)
+          return res.status(400).send('The path you inputted is incorrect, try again.\n' + err);
         }
-      })
-    })
-    return res.status(200).send('Successfully moved file(s).')
+      });
+    });
+    return res.status(200).send('Successfully moved file(s).');
   }
-})
+});
 
 // called whenever 'Delete' button is clicked with checkmarked files
 app.delete('/deleteMultiple', (req, res) => {
-  const files = req.body.fileNames
-  const listOfFilesExist = isArrayValid(files)
-  if (!listOfFilesExist) return res.status(400).send('There are no files to delete.')
+  const files = req.body.fileNames;
+  const listOfFilesExist = isArrayValid(files);
+  if (!listOfFilesExist) {
+    return res.status(400).send('There are no files to delete.');
+  }
 
-  files.forEach(file => {
-    const filePath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file))
-    const isDirectory = filePathExists(filePath) && fs.lstatSync(filePath).isDirectory()
+  files.forEach((file) => {
+    const filePath = nodePath.normalize(storagePath + removeEncodedUrlSpace(file));
+    const isDirectory = filePathExists(filePath) && fs.lstatSync(filePath).isDirectory();
     if (isDirectory) {
       fs.rmdir(filePath, { recursive: true }, (err) => {
         if (err) {
-          console.log('Error in deleting directory.')
-          return res.status(400).send('Error in deleting directory.\n' + err)
+          console.log('Error in deleting directory.');
+          return res.status(400).send('Error in deleting directory.\n' + err);
         } else {
-          console.log('Directory has been deleted successfully.')
-          return res.status(200).send()
+          console.log('Directory has been deleted successfully.');
+          return res.status(200).send();
         }
-      })
+      });
     } else {
       fs.unlink(filePath, (err) => {
         if (err) {
-          console.log('Error in deleting file(s).\n' + err)
-          return res.status(400).send('Error in deleting file(s).\n' + err)
+          console.log('Error in deleting file(s).\n' + err);
+          return res.status(400).send('Error in deleting file(s).\n' + err);
         }
-      })
+      });
     }
-  })
-  return res.status(200).send('File(s) have been deleted successfully.')
-})
+  });
+  return res.status(200).send('File(s) have been deleted successfully.');
+});
 
 // only gets called when you upload a file then click on the remove link attached to dropzone
-app.delete('/deleteFile', (req, res) => {
-  let pathToFile = ''
-  const fileName = req.body.fileName
-  if (!isValid(fileName)) return res.status(500).send('File does not exist.')
+app.delete('/deleteFile', checkUserAuth, (req, res) => {
+  let pathToFile = '';
+  const fileName = req.body.fileName;
+  if (!isValid(fileName)) {
+    return res.status(500).send('File does not exist.');
+  }
 
   if (isValid(req.query.path)) {
-    pathToFile = nodePath.normalize(storagePath + '/' + fileName)
+    pathToFile = nodePath.normalize(storagePath + '/' + fileName);
   } else {
-    pathToFile = nodePath.normalize(`${storagePath.replace('home', '')}${req.query.path.replace('?path=')}${fileName}`)
+    pathToFile = nodePath.normalize(`${storagePath.replace('home', '')}${req.query.path.replace('?path=')}${fileName}`);
   }
 
   fs.unlink(pathToFile, (err) => {
     if (err) {
-      return res.status(500).send(err)
+      return res.status(500).send(err);
     } else {
-      return res.status(200).send('File has been deleted successfully.')
+      return res.status(200).send('File has been deleted successfully.');
     }
-  })
-})
+  });
+});
 
 // MIDDLEWARE
-//////////////////////////////////////////////
 
 // checks for user authentication
 function checkUserAuth(req, res, next) {
-  if (req.session.isUserAuth) return next()
-  return res.status(403).redirect('/')
+  if (req.session.isUserAuth) {
+    return next();
+  }
+  return res.status(403).redirect('/');
 }
 
 // is user logged in
 function redirectToMenuIfLoggedIn(req, res, next) {
-  if (req.session.isUserAuth) return res.redirect('/menu')
-  return next()
+  if (req.session.isUserAuth) {
+    return res.redirect('/menu');
+  } else {
+    return next();
+  }
 }
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
