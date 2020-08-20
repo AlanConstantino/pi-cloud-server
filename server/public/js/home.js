d3ef33af7de1a311284881b9700a3cac6c12e30b
AlanConstantino <constantinoalan98@gmail.com>
2020-08-19 17:59:44 -0700

    Applied ESLint rules to files

--- server/public/js/home.js
+++ server/public/js/home.js
@@ -1,183 +1,234 @@
 // Basic DOM manipulation
-const cloneNode = (node) => node.cloneNode(true)
-const createElement = (type) => document.createElement(type)
-const addAttribute = (element, attribute, value) => element.setAttribute(attribute, value)
-const removeAttribute = (element, attribute) => element.removeAttribute(attribute)
+const cloneNode = (node) => node.cloneNode(true);
+const createElement = (type) => document.createElement(type);
+const addAttribute = (element, attribute, value) => element.setAttribute(attribute, value);
+const removeAttribute = (element, attribute) => element.removeAttribute(attribute);
 const removeAttributes = (element, attributes) => {
-    attributes.forEach(attribute => element.removeAttribute(attribute))
-}
-const appendNode = (root, node) => root.appendChild(node)
+  attributes.forEach((attribute) => element.removeAttribute(attribute));
+};
+const appendNode = (root, node) => root.appendChild(node);
 const addClasses = (element, cssClasses) => {
-    cssClasses.forEach(cssClass => element.classList.add(cssClass))
-}
+  cssClasses.forEach((cssClass) => element.classList.add(cssClass));
+};
 
 // Table helper methods
-const calculateKilobytes = (bytes) => (bytes / 1024).toFixed(2)
-const validateSize = (size) => (size === 'NaN' ? 'dir' : size)
+const calculateKilobytes = (bytes) => (bytes / 1024).toFixed(2);
+const validateSize = (size) => (size === 'NaN' ? 'dir' : size);
 const cloneRowAndRemoveAttributes = (row, attributes) => {
-    const clone = cloneNode(row)
-    removeAttributes(clone, attributes)
-    return clone
-}
+  const clone = cloneNode(row);
+  removeAttributes(clone, attributes);
+  return clone;
+};
 const createClickableName = (name) => {
-    const aTag = createElement('a')
-    addAttribute(aTag, 'href', (window.location.href + name))
-    aTag.innerText = name
-    return aTag
-}
+  const aTag = createElement('a');
+  addAttribute(aTag, 'href', (window.location.href + name));
+  aTag.innerText = name;
+  return aTag;
+};
 const createCheckbox = (id) => {
-    const inputTag = createElement('input')
-    addAttribute(inputTag, 'type', 'checkbox')
-    addAttribute(inputTag, 'id', id)
-    addAttribute(inputTag, 'name', `${window.location.pathname}${id}`)
-    addClasses(inputTag, ['checkbox'])
-    return inputTag
-}
+  const inputTag = createElement('input');
+  addAttribute(inputTag, 'type', 'checkbox');
+  addAttribute(inputTag, 'id', id);
+  addAttribute(inputTag, 'name', `${window.location.pathname}${id}`);
+  addClasses(inputTag, [ 'checkbox' ]);
+  return inputTag;
+};
 
-const exampleRow = document.getElementById('example-row')
-const tbody = document.getElementById('tbody')
-const listItems = [...document.getElementById('files').children]
+const exampleRow = document.getElementById('example-row');
+const tbody = document.getElementById('tbody');
+const listItems = [ ...document.getElementById('files').children ];
 
-listItems.forEach(item => {
-    const row = cloneRowAndRemoveAttributes(exampleRow, ['id', 'class'])
-    const [name, size, date] = [...item.children[0].children]
-    const [rowNum, rowName, rowSize, rowDate] = [...row.children]
-    const aTag = createClickableName(name.innerText)
-    const checkbox = createCheckbox(name.innerText)
-    const isHiddenDirectory = (name.innerText === '..') || (name.innerText === '.')
+listItems.forEach((item) => {
+  const row = cloneRowAndRemoveAttributes(exampleRow, [ 'id', 'class' ]);
+  const [ name, size, date ] = [ ...item.children[0].children ];
+  const [ rowNum, rowName, rowSize, rowDate ] = [ ...row.children ];
+  const aTag = createClickableName(name.innerText);
+  const checkbox = createCheckbox(name.innerText);
+  const isHiddenDirectory = (name.innerText === '..') || (name.innerText === '.');
 
-    appendNode(rowName, aTag)
-    rowSize.innerText = validateSize(calculateKilobytes(parseInt(size.innerText)))
-    rowDate.innerText = date.innerText
-    appendNode(tbody, row)
-    if (isHiddenDirectory) return
-    appendNode(rowNum, checkbox)
-})
+  appendNode(rowName, aTag);
+  rowSize.innerText = validateSize(calculateKilobytes(parseInt(size.innerText)));
+  rowDate.innerText = date.innerText;
+  appendNode(tbody, row);
+
+  if (isHiddenDirectory) {
+    return;
+  }
+
+  appendNode(rowNum, checkbox);
+});
 
 // checkbox helper functions
-const getAllCheckedBoxes = (checkboxes) => checkboxes.filter(box => box.checked)
-const checkedBoxesAreValid = (checkedBoxes) => ((checkedBoxes !== undefined) && (checkedBoxes.length !== 0))
+const getAllCheckedBoxes = (checkboxes) => checkboxes.filter((box) => box.checked);
+const checkedBoxesAreValid = (checkedBoxes) => ((checkedBoxes !== undefined) && (checkedBoxes.length !== 0));
 const saveCheckedBoxesAsAListOfPaths = (checkedBoxes) => {
-    const list = [];
-    const filePath = (window.location.pathname).replace('/home', '')
-    checkedBoxes.forEach(box => list.push(`${filePath}${box.id}`))
-    return list
-}
+  const list = [];
+  const filePath = (window.location.pathname).replace('/home', '');
+  checkedBoxes.forEach((box) => list.push(`${filePath}${box.id}`));
+  return list;
+};
 
 // API helper functions
-const createUrl = (parameter) => window.location.origin + parameter
+const createUrl = (parameter) => window.location.origin + parameter;
 const sendFileNamesToServer = async (url, data, method) => {
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
-    return await fetch(url, options)
-}
+  const options = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  return await fetch(url, options);
+};
 
-const checkboxForm = document.getElementById('checkbox-form')
-const checkboxes = [...document.getElementsByClassName('checkbox')]
+const checkboxForm = document.getElementById('checkbox-form');
+const checkboxes = [ ...document.getElementsByClassName('checkbox') ];
 
-const downloadBtn = document.getElementById('download-selected-btn')
+const downloadBtn = document.getElementById('download-selected-btn');
 downloadBtn.addEventListener('click', async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const checkedBoxes = getAllCheckedBoxes(checkboxes)
-    if (!checkedBoxesAreValid(checkedBoxes)) return alert('You must select at least one box.')
-    const listOfFileNames = saveCheckedBoxesAsAListOfPaths(checkedBoxes)
+  const checkedBoxes = getAllCheckedBoxes(checkboxes);
 
-    // sending fetch request to server
-    const url = createUrl('/download')
-    const data = { fileNames: listOfFileNames }
-    const response = await sendFileNamesToServer(url, data, 'POST')
-    if (response.status === 200) window.location.href = `${window.location.origin}/downloadZip`
-    if (response.status === 400) alert('Error in deleting file(s).')
+  if (!checkedBoxesAreValid(checkedBoxes)) {
+    return alert('You must select at least one box.');
+  }
 
-    // reset form
-    checkboxForm.reset()
-})
+  const listOfFileNames = saveCheckedBoxesAsAListOfPaths(checkedBoxes);
 
-const deleteBtn = document.getElementById('delete-btn')
+  // sending fetch request to server
+  const url = createUrl('/download');
+  const data = { fileNames: listOfFileNames };
+  const response = await sendFileNamesToServer(url, data, 'POST');
+  const serverMessage = await response.text();
+
+  if (response.status === 200) {
+    window.location.href = `${window.location.origin}/downloadZip`;
+    alert(serverMessage);
+  }
+
+  if (response.status === 400) {
+    alert(serverMessage);
+  }
+
+  checkboxForm.reset();
+});
+
+const deleteBtn = document.getElementById('delete-btn');
 deleteBtn.addEventListener('click', async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const checkedBoxes = getAllCheckedBoxes(checkboxes)
-    if (!checkedBoxesAreValid(checkedBoxes)) return alert('You must select at least one box.')
-    const listOfFileNames = saveCheckedBoxesAsAListOfPaths(checkedBoxes)
+  const checkedBoxes = getAllCheckedBoxes(checkboxes);
 
-    // sending data to backend
-    const url = createUrl('/deleteMultiple')
-    const data = { fileNames: listOfFileNames }
-    const response = await sendFileNamesToServer(url, data, 'DELETE')
-    if (response.status === 200) return location.reload()
-    if (response.status === 400) return alert('Error in deleting file(s).')
+  if (!checkedBoxesAreValid(checkedBoxes)) {
+    return alert('You must select at least one box.');
+  }
 
-    // reset form
-    checkboxForm.reset()
-})
+  const listOfFileNames = saveCheckedBoxesAsAListOfPaths(checkedBoxes);
 
-const selectAllBtn = document.getElementById('select-all-btn')
+  // sending data to backend
+  const url = createUrl('/deleteCheckmarked');
+  const data = { fileNames: listOfFileNames };
+  const response = await sendFileNamesToServer(url, data, 'DELETE');
+  const serverMessage = await response.text();
+
+  if (response.status === 200) {
+    location.reload();
+    return alert(serverMessage);
+  }
+
+  if (response.status === 400) {
+    return alert(serverMessage);
+  }
+
+  checkboxForm.reset();
+});
+
+const selectAllBtn = document.getElementById('select-all-btn');
 selectAllBtn.addEventListener('click', () => {
-    const checkedBoxes = getAllCheckedBoxes(checkboxes)
-    if (checkedBoxes.length > 0) {
-        checkedBoxes.forEach(box => box.checked = false)
-        return
-    }
-    checkboxes.forEach(box => box.checked = true)
-})
+  const checkedBoxes = getAllCheckedBoxes(checkboxes);
 
-const menuBtn = document.getElementById('menu-btn')
-menuBtn.addEventListener('click', () => window.location.href = createUrl('/menu'))
+  if (checkedBoxes.length > 0) {
+    checkedBoxes.forEach((box) => box.checked = false);
+    return;
+  }
 
-const uploadBtn = document.getElementById('upload-btn')
+  checkboxes.forEach((box) => box.checked = true);
+});
+
+const menuBtn = document.getElementById('menu-btn');
+menuBtn.addEventListener('click', () => window.location.href = createUrl('/menu'));
+
+const uploadBtn = document.getElementById('upload-btn');
 uploadBtn.addEventListener('click', () => {
-    const path = window.location.pathname
-    window.location.href = createUrl(`/upload/?path=${path}`)
-})
+  const path = window.location.pathname;
+  window.location.href = createUrl(`/upload/?path=${path}`);
+});
 
 
 const isValid = (input) => {
-    if (input === undefined || input === '' || input === null || !input) return false
-    return true
-}
+  if (input === undefined || input === '' || input === null) {
+    return false;
+  }
+  return true;
+};
 
 // sends GET request to '/createFolder' with dirName and path passed in the URL
-const createFolderModalForm = document.getElementById('create-folder-form-modal')
+const createFolderModalForm = document.getElementById('create-folder-form-modal');
 createFolderModalForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const dirName = document.getElementById('folder-name-modal').value.trim()
-    if (!isValid(dirName)) return alert(`'${dirName}' is not valid`)
-    const path = window.location.pathname
-    window.location.href = createUrl(`/createFolder/?path=${path}&dirName=${dirName}`)
-})
+  e.preventDefault();
+  const dirName = document.getElementById('folder-name-modal').value.trim();
 
-const moveToBtn = document.getElementById('move-to-btn')
+  if (!isValid(dirName)) {
+    return alert(`'${dirName}' is not valid`);
+  }
+
+  const path = window.location.pathname;
+  window.location.href = createUrl(`/createFolder/?path=${path}&dirName=${dirName}`);
+});
+
+const moveToBtn = document.getElementById('move-to-btn');
 moveToBtn.addEventListener('click', () => {
-    const checkedBoxes = getAllCheckedBoxes(checkboxes)
-    if (!checkedBoxesAreValid(checkedBoxes)) {
-        removeAttribute(moveToBtn, 'data-toggle')
-        return alert('You must select at least one box.')
+  const checkedBoxes = getAllCheckedBoxes(checkboxes);
+
+  if (!checkedBoxesAreValid(checkedBoxes)) {
+    removeAttribute(moveToBtn, 'data-toggle');
+    return alert('You must select at least one box.');
+  }
+
+  addAttribute(moveToBtn, 'data-toggle', 'modal');
+  const listOfFiles = saveCheckedBoxesAsAListOfPaths(checkedBoxes);
+
+  // the following event won't ever trigger if moveToBtn is never clicked,
+  // hence why it's nested in the moveToBtn 'click' event listener
+  const moveToModalForm = document.getElementById('move-to-form-modal');
+  moveToModalForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let destinationDir = document.getElementById('directory-path-modal').value.trim();
+
+    // make sure destinationDir is valid
+    if (!isValid(destinationDir)) {
+      return alert(`'${destinationDir}' is not valid`);
     }
-    addAttribute(moveToBtn, 'data-toggle', 'modal')
-    const listOfFiles = saveCheckedBoxesAsAListOfPaths(checkedBoxes)
 
-    // the following event won't ever trigger if moveToBtn is never clicked,
-    // hence why it's nested in the moveToBtn 'click' event listener
-    const moveToModalForm = document.getElementById('move-to-form-modal')
-    moveToModalForm.addEventListener('submit', async (e) => {
-        e.preventDefault()
-        let destinationDir = document.getElementById('directory-path-modal').value.trim()
-        if (!isValid(destinationDir)) return alert(`'${destinationDir}' is not valid`)   // make sure destinationDir is valid
-        if (!destinationDir.startsWith('/')) destinationDir = '/' + destinationDir       // make sure destinationDir starts with a '/'
+    // make sure destinationDir starts with a '/'
+    if (!destinationDir.startsWith('/')) {
+      destinationDir = '/' + destinationDir;
+    }
 
-        // sending data to backend
-        const url = createUrl('/moveTo')
-        const data = { destinationDir, listOfFiles }
-        const response = await sendFileNamesToServer(url, data, 'POST')
-        const serverMessage = await response.text()
-        if (response.status === 200) return location.reload()
-        if (response.status === 400) return alert(`${serverMessage}`)
-    })
-})
+    // sending data to backend
+    const url = createUrl('/moveTo');
+    const data = { destinationDir, listOfFiles };
+    const response = await sendFileNamesToServer(url, data, 'POST');
+    const serverMessage = await response.text();
+
+    if (response.status === 200) {
+      location.reload();
+      return alert(serverMessage);
+    }
+
+    if (response.status === 400) {
+      return alert(`${serverMessage}`);
+    }
+  });
+});
--- server/public/js/upload.js
+++ server/public/js/upload.js
@@ -1,46 +1,59 @@
 async function deleteFileFromServer(file) {
-    let url = `${location.origin}/deleteFile`
-    if (window.location.search !== undefined && window.location.search !== '') {
-        url += `/${window.location.search}`
-    }
-    const options = {
-        method: 'delete',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(file)
-    }
-    const response = await fetch(url, options)
-    return response
+  let url = `${location.origin}/deleteFile`;
+
+  if (window.location.search !== undefined && window.location.search !== '') {
+    url += `/${window.location.search}`;
+  }
+
+  const options = {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(file)
+  };
+
+  const response = await fetch(url, options);
+  return response;
 }
 
 // dropzone config
 Dropzone.options.dropzoneArea = {
-    paramName: 'uploaded-files',
-    addRemoveLinks: true,
-    maxFilesize: null,
-    removedfile: async function (file) {
-        // sends DELETE request to server to remove file
-        const response = await deleteFileFromServer({ 'fileName': file.name })
-        if (response.status === 200) console.log('Successfully deleted file.')
-        if (response.status === 500) console.log('Couldn\'t delete file.')
+  paramName: 'uploaded-files',
+  addRemoveLinks: true,
+  maxFilesize: null,
+  removedfile: async function (file) {
+    // sends DELETE request to server to remove file
+    const response = await deleteFileFromServer({ 'fileName': file.name });
+    const serverMessage = await response.text();
 
-        // deletes dropzone thumbnail
-        let _ref;
-        return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
-    },
-    dictDefaultMessage: 'Drop file(s) here or click to upload',
-    dictRemoveFile: 'Remove',
-    init: function () {
-        this.hiddenFileInput.setAttribute("webkitdirectory", true)
-    },
-    headers: { path: window.location.search }
+    if (response.status === 200) {
+      location.reload();
+      return alert(serverMessage);
+    }
+
+    if (response.status === 400) {
+      alert(serverMessage);
+    }
+
+    // deletes dropzone thumbnail
+    let _ref;
+    return ((_ref = file.previewElement) != null) ? _ref.parentNode.removeChild(file.previewElement) : void 0;
+  },
+  dictDefaultMessage: 'Drop file(s) here or click to upload',
+  dictRemoveFile: 'Remove',
+  init: function () {
+    this.hiddenFileInput.setAttribute('webkitdirectory', true);
+  },
+  headers: {
+    path: window.location.search
+  }
 };
 
-const url = (route) => `${location.origin}${route}`
+const url = (route) => `${location.origin}${route}`;
 
 // when you click on the home button, you go to the '/home' url route
-const homeButton = document.getElementById('home-btn')
-homeButton.addEventListener('click', async () => window.location.href = url('/home'))
+const homeButton = document.getElementById('home-btn');
+homeButton.addEventListener('click', async () => window.location.href = url('/home'));
 
 // when you click on the back button, you go to the '/menu' url route
-const backButton = document.getElementById('menu-btn')
-backButton.addEventListener('click', async () => window.location.href = url('/menu'))
+const backButton = document.getElementById('menu-btn');
+backButton.addEventListener('click', async () => window.location.href = url('/menu'));
